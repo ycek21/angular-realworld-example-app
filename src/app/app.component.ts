1be7f8ac4c8beeabbf98ebbe9eb648c5458ff1a1
Jacek Eichler <jacekeichler@gmail.com>
2023-05-17 15:00:23 +0200

    Introduced necessary changes to successfully run npm build:ssr

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from "@angular/core";
-
+import { Component, Inject, OnInit, PLATFORM_ID } from "@angular/core";
 import { UserService } from "./core";
+import {isPlatformBrowser} from '@angular/common'
+
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
 })
 export class AppComponent implements OnInit {
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService,
+    @Inject(PLATFORM_ID) private platformId,
+    
+    
+    ) {}
 
   ngOnInit() {
-    this.userService.populate();
+    if(isPlatformBrowser(this.platformId)){
+      this.userService.populate();
+    }
+    console.log('Hello from AppComponent Init :>> ', );
   }
 }
